83553bf2766d3924046717ad6d1260f8eb5b54c5
kjw <kjw@kjwui-MacBookAir.local>
2025-05-10 19:01:27 +0900

    천기초 제품 크론탭 설정값 즉시 반영하도록 변경

--- src/app/cron/cron.tsx
+++ src/app/cron/cron.tsx
@@ -1,6 +1,36 @@
-import cron from 'node-cron';
+import cron, { ScheduledTask } from 'node-cron';
+import fs from 'fs';
+import path from 'path';
 import { syncCgcProducts } from './SyncCGCProducts';
 
-cron.schedule('0 0 * * * ', async () => {
-    await syncCgcProducts();
+const configPath = path.resolve(__dirname, "../config/cron-config.json");
+
+let currentTask: ScheduledTask | null = null;
+
+function initCgcSyncCronConfig() {
+    const rawSyncData = fs.readFileSync(configPath, "utf-8");
+    const config = JSON.parse(rawSyncData);
+    const expression = config.cgcProductSync;
+
+    if (!cron.validate(expression)) {
+        console.log("유효하지 않은 크론식");
+        return;
+    }
+
+    if (currentTask) {
+        currentTask.stop();
+        console.log("이전 작업 중지.");
+    }
+
+    currentTask = cron.schedule(expression, async () => {
+        await syncCgcProducts();
+    });
+}
+initCgcSyncCronConfig();
+
+fs.watch(configPath, (eventType) => {
+    if (eventType === 'change') {
+        console.log("설정파일 변경 감지. 적용중...");
+        initCgcSyncCronConfig();
+    }
 });
