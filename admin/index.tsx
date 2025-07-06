b3608a946e0bc1a3fc339deafdf33ed27ceaa04c
kjw <kjw@kjwui-MacBookAir.local>
2025-07-06 17:32:46 +0900

    파일 사이즈 제한 수정4

--- admin/index.tsx
+++ admin/index.tsx
@@ -9,6 +9,7 @@ import uploadFileFeature from '@adminjs/upload'
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { UploadProvider } from './components/UploadProvider'
+import formidable from 'formidable'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -17,17 +18,12 @@ AdminJS.registerAdapter(AdminJSMongoose);
 
 const componentLoader = new ComponentLoader();
 
+// 타입 무시하고 재정의
+;(formidable.IncomingForm.prototype as any).maxFileSize = 10 * 1024 * 1024 * 1024 // 10GB
+
 const uploadFeature = uploadFileFeature({
   componentLoader,
   provider: new UploadProvider,
-  // provider: {
-  //   local: {
-  //     bucket: 'uploads/audio',
-  //     opts: {
-  //       baseUrl: undefined,
-  //     }
-  //   }
-  // },
   properties: {
     key: 'audioFileKey',
     mimeType: 'audioFileMimeType',
